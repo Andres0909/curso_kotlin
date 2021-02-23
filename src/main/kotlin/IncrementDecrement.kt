6a0776a29a1183df228527e4d103209000645590
Andres0909 <andresjosecaparrosnicolas0909@gmail.com>
2021-02-23 20:19:59 +0100

    Finished Tema02

---
+++ src/main/kotlin/IncrementDecrement.kt
@@ -0,0 +1,14 @@
+
+
+fun incrementDecrement() {
+    //incremento y decremento de variables
+    var monstruo: Int = 5
+    monstruo += 8
+    print(monstruo)
+
+    if monstruo % 2 {
+        monstruo = 50
+    } else {
+monstruo = 55
+}
+    var monstruo = 45
