879b91213d8ffc863d8a213aba7d078ba76d41df
Andres0909 <andresjosecaparrosnicolas0909@gmail.com>
2021-02-24 19:17:23 +0100

    Started operadores con tipos mixtos

--- src/main/kotlin/constantes.kt
+++ src/main/kotlin/constantes.kt
@@ -1,8 +1,22 @@
 fun main() {
+
+    //Inferencia de tipos
     val number: Int = 10
     val pi: Double = 3.1415
 
     print(number)
 
 
+    //Conversion de tipos
+    var integer: Int = 100
+    var integer02: Int = 200
+    var decimal: Double = 12.5
+
+    integer = integer02
+   var mosntruosPantall01: Int = 100
+    var moscaVoladoraTriple: Double = 30.00
+
+
+    mosntruosPantall01 = moscaVoladoraTriple.toInt()
+    print(mosntruosPantall01)
 }
