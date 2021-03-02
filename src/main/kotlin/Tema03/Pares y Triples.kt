ff3632a10c4bba28e572d4e717a3dcdd1d7253d7
Andres0909 <andresjosecaparrosnicolas0909@gmail.com>
2021-03-02 20:01:28 +0100

    Pare y triple

--- src/main/kotlin/Tema03/Pares y Triples.kt
+++ src/main/kotlin/Tema03/Pares y Triples.kt
@@ -1,2 +1,24 @@
 package Tema03
 
+fun main() {
+
+    //Pair
+    val coordinates: Triple<Int, Int, Int> = Triple(2,3,7)
+    val coordinatesInference = Pair (2,3)
+    val coordinates02 = Pair (12, 3)
+    val coordinatesWithTo = 2 to 3
+
+    val coordinatesDouble = Pair (2.3 , 4.4)
+
+    val monstruo01 = coordinates
+    val monstruo02 = coordinates02
+
+    //Acceso a los datos de un pair
+    val monstruo01_x1 = coordinates.first
+    val monstruo02_y1 = coordinates.second
+
+    //print(monstruo01_x1)
+    //print(monstruo01_y1)
+
+    va
+}
