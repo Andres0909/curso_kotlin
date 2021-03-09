09023bd85a8dbcb9b7e49eecebdf8db7a96a76c9
Andres0909 <andresjosecaparrosnicolas0909@gmail.com>
2021-03-09 19:37:37 +0100

    Starter Igual de cadenas

---
+++ src/main/kotlin/Tema04/flujocntrolbsico.kt
@@ -0,0 +1,42 @@
+package Tema04
+
+fun main() {
+
+    val yes: Boolean = true
+    val no = false
+
+    val doesOneEqualTwo = {1 == 2}
+    val finalValue = doesOneEqualTwo
+
+    val doesOneNotEqualTwo = {1 != 2}
+    val finalValue = doesOneNotEqualTwo
+
+    val alsoTrue = !(1 == 2)
+
+    print(alsoTrue)
+
+    val isOneGreaterThanTwo = (1 >= 2)
+    val isOneLessThanTwo = (1 <= 2)
+
+    /*
+    < Menor
+    > Mator a
+    == Comparacion si dos tipos son iguales
+    <= Menor o igual
+    >= Mayor o igual
+    != Si es distinto de
+     */
+    // Operadores Booleanos
+
+    val adn1: Boolean = false&& false
+    val adn2 = true && true
+    val adn3 = false && true
+    val adn4 = true && false
+
+    //Operador Booleanos OR
+
+    val or02 = true || true
+    val or03 = false || true
+    val or04 = true || false
+
+}
